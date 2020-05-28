8a9137186216f8c5b3b56e2e64ab4b4ada709dee
dir322 <botkiller4@yandex.ru>
2020-05-28 16:56:41 +0300

    [xrGame] Еще правки xrGame

--- src/xrGame/xrServer.cpp
+++ src/xrGame/xrServer.cpp
@@ -141,9 +141,7 @@ void		xrServer::client_Destroy	(IClient* C)
 	// Delete assosiated entity
 	// xrClientData*	D = (xrClientData*)C;
 	// CSE_Abstract* E = D->owner;
-	IClient* alife_client = net_players.FindAndEraseClient(
-		std::bind1st(std::equal_to<IClient*>(), C)
-	);
+	IClient* alife_client = net_players.FindAndEraseClient(std::bind(std::equal_to<IClient*>(), std::placeholders::_1, C));
 	//VERIFY(alife_client);
 	if (alife_client)
 	{
