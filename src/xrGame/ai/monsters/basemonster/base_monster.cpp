6c1573755cb726cf8a4dadac573197cc955ae5bd
dir322 <botkiller4@yandex.ru>
2020-05-29 02:16:31 +0300

    [xrGame] "Временно" исправлен вылет со звуками мутантов.

--- src/xrGame/ai/monsters/basemonster/base_monster.cpp
+++ src/xrGame/ai/monsters/basemonster/base_monster.cpp
@@ -576,12 +576,12 @@ void CBaseMonster::set_state_sound(u32 type, bool once)
 			case MonsterSound::eMonsterSoundIdle : 
 				// check distance to actor
 
-				if (Actor()->Position().distance_to(Position()) > db().m_fDistantIdleSndRange) {
+				//if (Actor()->Position().distance_to(Position()) > db().m_fDistantIdleSndRange) {
 					delay = u32(float(db().m_dwDistantIdleSndDelay) * _sqrt(float(objects_count)));
 					type  = MonsterSound::eMonsterSoundIdleDistant;
-				} else {
-					delay = u32(float(db().m_dwIdleSndDelay) * _sqrt(float(objects_count)));
-				}
+				//} else {
+				//	delay = u32(float(db().m_dwIdleSndDelay) * _sqrt(float(objects_count)));
+				//}
 				
 				break;
 			case MonsterSound::eMonsterSoundEat:
