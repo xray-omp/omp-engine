81cc0627bf2d71b4423bf346f670410eca2b9f86
Pavel Spitsin <pspitsin@tamashi.games>
2020-09-14 16:40:21 +0300

    [xrGame] base_monster.cpp - исправлена логика для синглплеера

--- src/xrGame/ai/monsters/basemonster/base_monster.cpp
+++ src/xrGame/ai/monsters/basemonster/base_monster.cpp
@@ -579,15 +579,21 @@ void CBaseMonster::set_state_sound(u32 type, bool once)
 			u32 delay = 0;
 			switch (type) {
 			case MonsterSound::eMonsterSoundIdle : 
-				// check distance to actor
-
-				//if (Actor()->Position().distance_to(Position()) > db().m_fDistantIdleSndRange) {
+				if (IsGameTypeSingle())
+				{
+					if (Actor()->Position().distance_to(Position()) > db().m_fDistantIdleSndRange) {
+						delay = u32(float(db().m_dwDistantIdleSndDelay) * _sqrt(float(objects_count)));
+						type = MonsterSound::eMonsterSoundIdleDistant;
+					}
+					else {
+						delay = u32(float(db().m_dwIdleSndDelay) * _sqrt(float(objects_count)));
+					}
+				}
+				else
+				{
 					delay = u32(float(db().m_dwDistantIdleSndDelay) * _sqrt(float(objects_count)));
-					type  = MonsterSound::eMonsterSoundIdleDistant;
-				//} else {
-				//	delay = u32(float(db().m_dwIdleSndDelay) * _sqrt(float(objects_count)));
-				//}
-				
+					type = MonsterSound::eMonsterSoundIdleDistant;
+				}
 				break;
 			case MonsterSound::eMonsterSoundEat:
 				delay = u32(float(db().m_dwEatSndDelay) * _sqrt(float(objects_count)));
