5617e8e52f953ddfd02a7efe509d7412f1896b09
Pavel Spitsin <pspitsin@tamashi.games>
2021-03-16 21:27:05 +0300

    [xrGame] реализован дроп рюкзака с лутом в режиме RP

--- src/xrGame/game_sv_roleplay.cpp
+++ src/xrGame/game_sv_roleplay.cpp
@@ -39,4 +39,55 @@ void game_sv_roleplay::OnPlayerReady(ClientID id_who)
 	// process GAME_EVENT_PLAYER_READY
 	inherited::OnPlayerReady(id_who);
 }
+
+void game_sv_roleplay::RespawnPlayer(ClientID id_who, bool NoSpectator)
+{
+	inherited::RespawnPlayer(id_who, NoSpectator);
+
+	xrClientData*	xrCData = (xrClientData*)m_server->ID_to_client(id_who);
+	if (!xrCData) return;
+
+	game_PlayerState*	ps = xrCData->ps;
+	if (!ps) return;
+
+	CSE_ALifeCreatureActor	*pA = smart_cast<CSE_ALifeCreatureActor*>(xrCData->owner);
+	if (!pA) return;
+	
+	SpawnWeapon4Actor(pA->ID, "mp_players_rukzak", 0, ps->pItemList);
+}
+
+BOOL game_sv_roleplay::OnTouch(u16 eid_who, u16 eid_what, BOOL bForced)
+{
+	CSE_ActorMP *e_who = smart_cast<CSE_ActorMP*>(m_server->ID_to_entity(eid_who));
+	if (!e_who)
+		return TRUE;
+
+	CSE_Abstract *e_entity = m_server->ID_to_entity(eid_what);
+	if (!e_entity)
+		return FALSE;
+
+	// pick up players bag
+	if (e_entity->m_tClassID == CLSID_OBJECT_PLAYERS_BAG)
+	{
+		return OnTouchPlayersBag(e_who, e_entity);
+	}
+
+	return TRUE;
+}
+
+void game_sv_roleplay::OnDetach(u16 eid_who, u16 eid_what)
+{
+	CSE_ActorMP *e_who = smart_cast<CSE_ActorMP*>(m_server->ID_to_entity(eid_who));
+	if (!e_who)
+		return;
+
+	CSE_Abstract *e_entity = m_server->ID_to_entity(eid_what);
+	if (!e_entity)
+		return;
+	
+	// drop players bag
+	if (e_entity->m_tClassID == CLSID_OBJECT_PLAYERS_BAG)
+	{
+		OnDetachPlayersBag(e_who, e_entity);
+	}
 }
--- src/xrGame/game_sv_roleplay.h
+++ src/xrGame/game_sv_roleplay.h
@@ -15,7 +15,16 @@ public:
 
 	virtual		void				OnPlayerSelectTeam(NET_Packet& P, ClientID sender);
 	virtual		void				OnPlayerReady(ClientID id_who);
+	virtual		void				RespawnPlayer(ClientID id_who, bool NoSpectator);
 
 	virtual		void				OnEvent(NET_Packet &tNetPacket, u16 type, u32 time, ClientID sender);
+	
+	virtual		BOOL				OnTouch(u16 eid_who, u16 eid_what, BOOL bForced = FALSE);
+	virtual		void				OnDetach(u16 eid_who, u16 eid_what);
+
+	// drop items after death
+	virtual		void				FillDeathActorRejectItems(CSE_ActorMP *actor, xr_vector<CSE_Abstract*> & to_reject);
+						BOOL				OnTouchPlayersBag(CSE_ActorMP *actor, CSE_Abstract *item);
+						void				OnDetachPlayersBag(CSE_ActorMP *actor, CSE_Abstract *item);
 };
 
