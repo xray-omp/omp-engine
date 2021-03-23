684c99925584ada574554968a4f09c9db8459acd
Pavel Spitsin <pspitsin@tamashi.games>
2021-03-23 21:13:45 +0300

    [xrGame] console_commands.cpp - добавлена команда fov для release сборки

--- src/xrGame/console_commands.cpp
+++ src/xrGame/console_commands.cpp
@@ -1838,8 +1838,9 @@ void CCC_RegisterCommands()
 
 #ifdef DEBUG
 	CMD4(CCC_Float,				"hud_fov",				&psHUD_FOV,		0.1f,	1.0f);
-	CMD4(CCC_Float,				"fov",					&g_fov,			5.0f,	180.0f);
 #endif // DEBUG
+
+	CMD4(CCC_Float, "fov", &g_fov, 5.0f, 180.0f);
 
 	// Demo
 #if 1//ndef MASTER_GOLD
